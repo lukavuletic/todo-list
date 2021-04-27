9845e6207774b3abdb6299091417a6075af8e6ff
lvuletic <luka.vuletic1999@gmail.com>
2021-04-27 07:46:31 +0200

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container" >
+      <header>
+        to-do list
+      </header>
+    </div>
+  );
+}
+
+export default App;
